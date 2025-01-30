66b092bb588a704c4e4362267f34dac774184548
jasiah Ortega <jortega6178@neisd.net>
2025-01-30 13:37:51 -0600

    sigma
    Co-authored-by: Owen Crace <coindxter@users.noreply.github.com>
    Co-authored-by: BigMacT <BigMacT@users.noreply.github.com>

--- src/Property.java
+++ src/Property.java
@@ -8,7 +8,6 @@ public class Property {
     private  int numHouses;
     private final int mortgage;
     private final int unmortgage;
-    private  int baseRent;
     private  int[] houseRents;
     private  boolean isRailroad;
     private  boolean isUtility;
@@ -16,7 +15,7 @@ public class Property {
     private boolean isChance;
     private boolean isCommunityChest;
 
-    public Property(Color c, String n, String o, int nH, int m, int um, int bR, int[] hR, boolean iR, boolean iU, boolean iP, boolean iC, boolean iCC)
+    public Property(Color c, String n, String o, int nH, int m, int um, int[] hR, boolean iR, boolean iU, boolean iP, boolean iC, boolean iCC)
     {
 
         color = c;
@@ -25,7 +24,6 @@ public class Property {
         numHouses = nH;
         mortgage = m;
         unmortgage = um;
-        baseRent = bR;
         houseRents = hR;
         isRailroad = iR;
         isUtility = iU;
