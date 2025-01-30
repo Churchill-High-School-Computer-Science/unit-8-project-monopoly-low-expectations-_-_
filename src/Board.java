59f89c8d1632f926faed6ea60c684597ea131c5b
jasiah Ortega <jortega6178@neisd.net>
2025-01-30 13:03:32 -0600

    hgjgjhgjh

--- src/Board.java
+++ src/Board.java
@@ -8,7 +8,7 @@ public class Board {
 
         Display.properties= properties;
 
-        properties[10][10] = new Property(new Color(205, 230, 208), "Go", "0", 0, 0);
+        properties[10][10] = new Property(new Color(102, 102, 102), "Go", "0", 0, 0);
         properties[10][9] = new Property(new Color(0, 114, 187), "Boardwalk", "400", 200, 220);
         properties[10][8] = new Property(new Color(205, 230, 208), "Luxury Tax", "100", 0, 0);
         properties[10][7] = new Property(new Color(0, 114, 187), "Park Place", "350", 175, 193);
@@ -18,7 +18,7 @@ public class Board {
         properties[10][3] = new Property(new Color(205, 230, 208), "Community Chest", "0", 0, 0);
         properties[10][2] = new Property(new Color(31, 178, 90), "North Carolina Avenue", "300", 150, 165);
         properties[10][1] = new Property(new Color(31, 178, 90), "Pacific Avenue", "300", 150, 165);
-        properties[10][0] = new Property(new Color(205, 230, 208), "Go to Jail", "0", 0, 0);
+        properties[10][0] = new Property(new Color(102, 102, 102), "Go to Jail", "0", 0, 0);
 
         properties[9][0] = new Property(new Color(254, 242, 0), "Marvin Gardens", "280", 140, 154);
         properties[8][0] = new Property(new Color(255, 255, 255), "Water Works", "150", 75, 83);
@@ -29,7 +29,7 @@ public class Board {
         properties[3][0] = new Property(new Color(237, 27, 36), "Indiana Avenue", "220", 110, 121);
         properties[2][0] = new Property(new Color(205, 230, 208), "Chance", "0", 0, 0);
         properties[1][0] = new Property(new Color(237, 27, 36), "Kentucky Avenue", "220", 110, 121);
-        properties[0][0] = new Property(new Color(205, 230, 208), "Free Parking", "0", 0, 0);
+        properties[0][0] = new Property(new Color(102, 102, 102), "Free Parking", "0", 0, 0);
 
         properties[0][1] = new Property(new Color(247, 148, 29), "New York Avenue", "200", 100, 110);
         properties[0][2] = new Property(new Color(247, 148, 29), "Tennessee Avenue", "180", 90, 99);
@@ -40,7 +40,7 @@ public class Board {
         properties[0][7] = new Property(new Color(217, 58, 150), "States Avenue", "140", 70, 77);
         properties[0][8] = new Property(new Color(255, 255, 255), "Electric Company", "150", 75, 83);
         properties[0][9] = new Property(new Color(217, 58, 150), "St. Charles Place", "140", 70, 77);   
-        properties[0][10] = new Property(new Color(205, 230, 208), "Jail", "0", 0, 0);
+        properties[0][10] = new Property(new Color(102, 102, 102), "Jail", "0", 0, 0);
 
         properties[1][10] = new Property(new Color(170, 224, 250), "Connecticut Avenue", "120", 60, 66);
         properties[2][10] = new Property(new Color(170, 224, 250), "Vermont Avenue", "100", 50, 55);
